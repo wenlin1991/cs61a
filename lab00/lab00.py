f621d2a1a94e36a3c3c08ee3085de9664dab5264
Wen Lin <linwen1991@gmail.com>
2016-02-19 09:47:01 -0800

    solution for lab00

---
+++ lab00/lab00.py
@@ -0,0 +1,9 @@
+def twenty_sixteen():
+    """Come up with the most creative expression that evaluates to 2016,
+    using only numbers and the +, *, and - operators.
+
+    >>> twenty_sixteen()
+    2016
+    """
+    value = (504 + 504) * 2
+    return value
