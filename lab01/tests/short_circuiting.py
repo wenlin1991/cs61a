0217c847fcb44db54556db46ecaed9a686d2b566
Wen Lin <linwen1991@gmail.com>
2016-02-19 09:47:29 -0800

    solution for lab01

---
+++ lab01/tests/short_circuiting.py
@@ -0,0 +1,44 @@
+test = {
+  'name': 'Veritasiness',
+  'points': 0,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> True and 13
+          13
+          >>> False or 0
+          0
+          >>> not 10
+          False
+          >>> not None
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        },
+        {
+          'code': r"""
+          >>> True and 1 / 0 and False # If this errors, just type Error.
+          Error
+          >>> True or 1 / 0 or False # If this errors, just type Error.
+          True
+          >>> True and 0 # If this errors, just type Error.
+          0
+          >>> False or 1 # If this errors, just type Error.
+          1
+          >>> 1 and 3 and 6 and 10 and 15 # If this errors, just type Error.
+          15
+          >>> 0 or False or 2 or 1 / 0 # If this errors, just type Error.
+          2
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': False,
+      'type': 'wwpp'
+    }
+  ]
+}
